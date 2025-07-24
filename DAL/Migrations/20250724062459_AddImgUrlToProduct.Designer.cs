7430cff508278d0a9c9b7848a06861d41ad08126
MinhTuan0269 <mtpham0246@gmail.com>
2025-07-24 13:25:47 +0700

    Update ImgUrl

--- DAL/Migrations/20250724062459_AddImgUrlToProduct.Designer.cs
+++ DAL/Migrations/20250724062459_AddImgUrlToProduct.Designer.cs
@@ -4,6 +4,7 @@ using DAL.DBContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DAL.Migrations
 {
     [DbContext(typeof(eStoreDbContext))]
-    partial class eStoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250724062459_AddImgUrlToProduct")]
+    partial class AddImgUrlToProduct
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- DAL/Models/Product.cs
+++ DAL/Models/Product.cs
@@ -14,6 +14,7 @@ namespace DAL.Models
         public int CategoryId { get; set; }
         public string ProductName { get; set; }
         public string Weight { get; set; }
+        public string ImgUrl { get; set; }
         public decimal UnitPrice { get; set; }
         public int UnitsInStock { get; set; }
         public bool Status { get; set; } = true;
